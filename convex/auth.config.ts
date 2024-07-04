8ef33e352f7bf89684980599a31166e11cb0c6eb
PeicoPaco <rodrigo.reategui.g@gmail.com>
2024-07-04 17:59:25 -0500

    first deployment incoming

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://liked-tarpon-98.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
